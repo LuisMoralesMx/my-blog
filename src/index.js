c3a5dc1be4a3899a97380451a53614bcadba8b28
Luis Morales <icluis.morales@americanwell.com>
2024-01-24 08:57:45 -0600

    Initial commit for React Workshop

--- src/index.js
+++ src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDepB3l2f-YD_mynZh-AA4rzS2X406q30w",
+  authDomain: "my-react-blog-fd963.firebaseapp.com",
+  projectId: "my-react-blog-fd963",
+  storageBucket: "my-react-blog-fd963.appspot.com",
+  messagingSenderId: "898956467816",
+  appId: "1:898956467816:web:0ee4eb6160fce54bd565dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
